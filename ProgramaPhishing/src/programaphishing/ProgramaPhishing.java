af1afcc357c3e3327f3fd4d54f8f5331cc91a034
EstuardoSalazar9961 <149229123+EstuardoSalazar9961@users.noreply.github.com>
2024-01-02 21:25:04 -0500

    palabras o frases

--- ProgramaPhishing/src/programaphishing/ProgramaPhishing.java
+++ ProgramaPhishing/src/programaphishing/ProgramaPhishing.java
@@ -5,6 +5,9 @@
  */
 package programaphishing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author CompuStore
@@ -15,7 +18,38 @@ public class ProgramaPhishing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Map<String, Integer> keywords = new HashMap<>();
+        keywords.put("actualización de seguridad", 2);
+        keywords.put("verificación de cuenta", 3);
+        keywords.put("su cuenta ha sido comprometida", 3);
+        keywords.put("gane dinero fácil y rápido", 3);
+        keywords.put("restablecimiento de contraseña urgente", 2);
+        keywords.put("gane un premio increíble", 3);
+        keywords.put("gestión de la cuenta", 2);
+        keywords.put("inicie sesión para evitar la suspensión", 3);
+        keywords.put("oferta exclusiva", 2);
+        keywords.put("problemas con su cuenta", 2);
+        keywords.put("¡felicidades, usted ha ganado!", 3);
+        keywords.put("confirme su información", 3);
+        keywords.put("importante mensaje de seguridad", 2);
+        keywords.put("promoción especial", 2);
+        keywords.put("gane un iPhone gratis", 3);
+        keywords.put("cierre de cuenta inminente", 3);
+        keywords.put("actualización de la política de privacidad", 2);
+        keywords.put("oferta limitada", 2);
+        keywords.put("inicio de sesión desde una ubicación no reconocida", 3);
+        keywords.put("gratis", 2);
+        keywords.put("su cuenta será suspendida", 3);
+        keywords.put("acceda a su cuenta ahora", 3);
+        keywords.put("¡solo por hoy!", 2);
+        keywords.put("urgente: su cuenta está en riesgo", 3);
+        keywords.put("revise su cuenta", 2);
+        keywords.put("increíble oportunidad de inversión", 3);
+        keywords.put("gane dinero desde casa", 3);
+        keywords.put("verificación de identidad", 3);
+        keywords.put("oferta de trabajo desde casa", 3);
+        keywords.put("su cuenta será cerrada", 3);
     }
     
 }
